e299b1bf295b794c47a8900829b4debb49ab4340
wAlienUFOx <anisimov.valera2000@yandex.ru>
2025-03-14 07:04:43 +0300

    cache for characters

--- lib/data/data_sources/local/shared_prefs.dart
+++ lib/data/data_sources/local/shared_prefs.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'package:rick_and_morty/data/models/character_model.dart';
+import 'package:rick_and_morty/data/models/response_model.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class SharedPrefs {
@@ -18,9 +19,20 @@ class SharedPrefs {
   static bool get darkTheme => sharedPreferences.getBool(SharedPrefsNames.darkTheme) ?? false;
 
   static set darkTheme(bool darkTheme) => sharedPreferences.setBool(SharedPrefsNames.darkTheme, darkTheme);
+
+  /// сохранённые персонажи
+  static ResponseModel? getCached(int page) {
+    final String? cachedString = sharedPreferences.getString('SharedPrefsNames.cachedCharacters$page');
+    if (cachedString == null) return null;
+    return ResponseModel.fromJson(json.decode(cachedString));
+  }
+
+  static setCached(int page, ResponseModel value) =>
+      sharedPreferences.setString('SharedPrefsNames.cachedCharacters$page', json.encode(value.toJson()));
 }
 
 class SharedPrefsNames {
   static String favorites = 'favorites';
   static String darkTheme = 'darkTheme';
+  static String cachedCharacters = 'cachedCharacters_';
 }
--- lib/data/repositories/characters_repository_impl.dart
+++ lib/data/repositories/characters_repository_impl.dart
@@ -1,3 +1,5 @@
+import 'package:dio/dio.dart';
+import 'package:rick_and_morty/data/data_sources/local/shared_prefs.dart';
 import 'package:rick_and_morty/data/data_sources/remote/characters_api_client.dart';
 import 'package:rick_and_morty/data/models/response_model.dart';
 import 'package:rick_and_morty/domain/repositories/characters_repository.dart';
@@ -9,6 +11,16 @@ class CharactersRepositoryImpl implements CharactersRepository {
 
   @override
   Future<ResponseModel> getCharacters(int page) async {
-    return await _charactersApiClient.getCharacters(page);
+    try {
+      final response =  await _charactersApiClient.getCharacters(page);
+      SharedPrefs.setCached(page, response);
+      return response;
+    } on DioException {
+      final cachedCharacters = SharedPrefs.getCached(page);
+      if (cachedCharacters != null) return cachedCharacters;
+      rethrow;
+    } catch (e) {
+      rethrow;
+    }
   }
 }
